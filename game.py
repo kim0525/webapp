f30ab583edcb4dc4d20a00c446a59df26f10f8c4
kim <parable321@gmail.com>
2021-05-17 12:48:23 +0900

    d

---
+++ game.py
@@ -0,0 +1,13 @@
+#import test
+from test import ifpung
+
+print("hello world 함수 사용")
+
+print("게임시작")
+print("적이 나타났습니다.")
+print("어떻게 할래?  1.펑  2.뿡  3. 휙")
+
+inp = input("입력: ")
+ifpung(inp)
+
+print("결과는 이렇게 되었습니다.")
